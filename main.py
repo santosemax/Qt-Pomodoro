4a1ba22df7d957bfa0824611de032a0d966a12d0
Max Santos <maxedwardsantos@gmail.com>
2021-08-01 11:31:25 -0400

    Added break/edit functions

--- main.py
+++ main.py
@@ -1,32 +1,76 @@
 #!/usr/local/bin/python3
-import sys, time, pdb
+import sys, time 
 from PyQt5 import QtWidgets as qtw   # Widgets/Layout Classses
-from PyQt5 import QtCore as qtc      # Contains signals and slots
-from PyQt5 import QtGui as qtg       # Other gui classes (fonts/colors/etc)
-from PyQt5 import uic                # Import ui File
+from PyQt5 import QtCore as qtc      # Contains signals and slots from PyQt5 import QtGui as qtg       
+from PyQt5 import uic                # (Fonts/colors/etc) (Unused)
 from design import Ui_Form
+from editTime import Ui_Form as editWin
 
-class Signals(qtc.QObject):
+# Handles all timer functions
+class Thread(qtc.QThread):
     time_signal = qtc.pyqtSignal(list)
 
-class Thread(qtc.QRunnable):
-    signal = qtc.pyqtSignal(int)
+    active = False
+    isStarted = False
+    break_active = False
+    rotationTime = 25
+    breakTime = 5
+    longTime = 15
+    Min, Sec = rotationTime, 0
+    progress = 0
 
-    def __init__(self):
-        super(Thread, self).__init__()
-        self.signal = Signals()
-
-    @qtc.pyqtSlot()
     def run(self):
-        Min, Sec = 24, 60
-        while Min != 0:
-            Sec -= 1
+        while self.Min >= 0 and self.isStarted: 
+            self.active = True
+            self.Sec -= 1
+            if self.Sec == -1:
+                self.Min -= 1
+                self.Sec = 59
+                # Break Logic
+                if self.Min < 0:
+                    self.break_active = not self.break_active
+                    if self.break_active == True:
+                        self.Min = self.breakTime
+                        self.Sec = 0
+                        self.isStarted = False
+                        print("-- Break Time Started --")
+                    else:
+                        self.Min = self.rotationTime
+                        self.Sec = 0
+                        self.isStarted = False
+                        print("-- Break Time Ended --")
+            self.time_signal.emit([self.Min, self.Sec])
+            if self.break_active != True:
+                self.progress += 1
             time.sleep(1.0)
-            if Sec == -1:
-                Min -= 1
-                Sec = 59
-            self.signal.time_signal.emit([Min, Sec])
 
+# Edit Timer Window
+class editWindow(qtw.QWidget):
+    time_variables =  qtc.pyqtSignal(list)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ui = editWin()
+        self.ui.setupUi(self) 
+        self.setFixedSize(372, 105)
+        self.setWindowTitle("Configure Timer")
+        self.setWindowFlags(qtc.Qt.WindowStaysOnTopHint | qtc.Qt.FramelessWindowHint)
+
+        self.ui.cancel_button.clicked.connect(self.exit)
+        self.ui.apply_button.clicked.connect(self.apply)
+    
+
+    def apply(self):
+        self.time_variables.emit([
+            self.ui.rotation_time.currentText()[0:2],
+            self.ui.break_time.currentText()[0:2],
+            self.ui.long_break_time.currentText()[0:2]
+        ])
+        # print(f"{int(self.ui.long_break_time.currentText()[0:2])}") # Debug Line
+        self.close()
+
+    def exit(self):
+        self.close()
 
 
 class MainWindow(qtw.QWidget):
@@ -34,54 +78,76 @@ class MainWindow(qtw.QWidget):
         super().__init__(*args, **kwargs)
         # Code Starts Here
         self.ui = Ui_Form()
-        self.ui.setupUi(self) # setupUi builds the GUI we designed onto the QWidget
+        self.ui.setupUi(self) 
         self.setFixedSize(481, 196)
         self.setWindowTitle("QT Pomo - Timer")
-        self.threadpool = qtc.QThreadPool()
+        self.thread = Thread()
+        self.w = editWindow()
+        self.active = False
 
-        # Button Functions
-        #self.ui.start_button.clicked.connect(self.thread)
-        self.ui.start_button.clicked.connect(self.click_start)
-
+        self.ui.start_button.clicked.connect(self.start_timer)
+        self.ui.pause_button.clicked.connect(self.stop_timer)
+        self.ui.reset_button.clicked.connect(self.reset_timer)
+        self.ui.edit_button.clicked.connect(self.edit_timer)
+        self.ui.pause_button.setEnabled(False)
+        self.ui.progressBar.setMaximum((self.thread.Min * 60) * 3)
+        
         # Code ends here
         self.show()
 
-    # Timer Signal (using class above)
-    def click_start(self):
-        thread = Thread()
-        thread.signal.time_signal.connect(self.function_thread)
-        self.threadpool.start(thread)
+    # Timer signal (using class above)
+    def start_timer(self):
+        if self.thread.active == False: 
+            self.thread.time_signal.connect(self.function_thread)
+        self.thread.start()
+        self.thread.isStarted = True
+        #self.ui.start_button.setEnabled(False)
+        self.ui.pause_button.setEnabled(True)
+
+    # Stop signal to pause the timer
+    def stop_timer(self):
+        self.thread.isStarted = False
+        self.ui.start_button.setEnabled(True)
+        self.ui.pause_button.setEnabled(False)
+        if self.thread.isStarted == True:
+            self.ui.start_button.setText("Resume")
+
+    def reset_timer(self):
+        self.thread.Min = 25
+        self.thread.Sec = 0
+        self.ui.timer.setText(f"{self.thread.Min}:{self.thread.Sec:02}")
+        self.ui.start_button.setEnabled(True)
+        self.ui.pause_button.setEnabled(False)
+        self.ui.start_button.setText("Start")
+
+    def edit_timer(self):
+        self.w.show()
+        # Connecting Signals to Slot
+        self.w.time_variables.connect(self.edit_function)
 
     # Timer Slot
-    def function_thread(self, signal):
+    def function_thread(self, signal): 
         self.ui.timer.setText(f"{signal[0]}:{signal[1]:02}")
-        print(signal)
+        self.ui.progressBar.setValue(self.thread.progress)
+        print(signal) # Debug Line
 
-    # Thread Here:
-     
+    # Edit Time Slot
+    def edit_function(self, variables):
+        self.thread.Min = int(variables[0])
+        self.thread.rotationTime = int(variables[0])
+        self.thread.breakTime = int(variables[1])
+        self.thread.longTime = int(variables[2])
+        self.thread.Sec = 0
+        print(f"Rotation Time: {variables[0]}, Break Time: {variables[1]}, Long Time:  {variables[2]}") # Debug Line
+        self.ui.timer.setText(f"{int(variables[0])}:00")
+        self.thread.isStarted = False
+        self.ui.start_button.setEnabled(True)
+        self.ui.pause_button.setEnabled(False)
+        self.ui.start_button.setText("Start")
 
-
-
-
-    # Thread for Timer Start
-    # def thread(self):
-    #     t1 = Thread(target=self.start_timer)
-    #     t1.start()
-
-
-    # Slot for Starting the Timer (Needs to be Threaded or will freeze)
-    #def start_timer(self):
-    #    Min, Sec = 24, 60
-    #    while Min != 0:
-    #        Sec -= 1
-    #        time.sleep(1.0)
-    #        if Sec == -1:
-    #            Min -= 1
-    #            Sec = 59
-    #        self.ui.timer.setText(f"{Min}:{Sec:02}")
 
 
 if __name__ == '__main__':
     app = qtw.QApplication(sys.argv)
-    w = MainWindow()
+    w = MainWindow()    
     sys.exit(app.exec_())
